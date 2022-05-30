b66f1a0a0aae46cf1e6324cb0238c045ee23af63
Oleg Izyumov <bukvoezhka@gmail.com>
2022-05-31 00:47:46 +0300

    Add archive report category to admin panel

--- simple_task_manager/main/admin.py
+++ simple_task_manager/main/admin.py
@@ -5,7 +5,7 @@ from simple_task_manager.main.models import ArchiveReport, Event, Category
 class EventAdmin(admin.ModelAdmin):
     list_display = ('description', 'category', 'created_at', 'updated_at', 'archive_report')
     list_display_links = ('description',)
-    list_editable = ('category',)
+    list_editable = ('category', 'archive_report',)
 
 
 admin.site.register(Event, EventAdmin)
